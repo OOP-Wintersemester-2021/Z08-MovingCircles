c430b7070c37e7c52c20a798b144983e8118e1f5
Alexander Bazo <alexanderbazo@googlemail.com>
2021-11-07 13:25:32 +0100

    Prepare starter code

--- src/MovingCirclesApp.java
+++ src/MovingCirclesApp.java
@@ -1,8 +1,7 @@
 import de.ur.mi.oop.app.GraphicsApp;
 import de.ur.mi.oop.colors.Color;
-import de.ur.mi.oop.graphics.Circle;
+import de.ur.mi.oop.graphics.*;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
-
 
 public class MovingCirclesApp extends GraphicsApp {
 
@@ -15,59 +14,15 @@ public class MovingCirclesApp extends GraphicsApp {
 
     private static final int WINDOW_WIDTH = 500;
     private static final int WINDOW_HEIGHT = 500;
-    private static final int CIRCLE_RADIUS = 15;
-    private static final int CIRCLE_MOVEMENT_RADIUS = 200;
-
-    private Circle movingCircle;
-    private double currentCirclePositionInDegree = 0;
 
     @Override
     public void initialize() {
         setCanvasSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        movingCircle = createCircle();
-    }
-
-    private Circle createCircle() {
-        int x = WINDOW_WIDTH / 2;
-        int y = (WINDOW_HEIGHT - (2 * CIRCLE_MOVEMENT_RADIUS)) / 2;
-        return new Circle(x, y, CIRCLE_RADIUS, RED);
     }
 
     @Override
     public void draw() {
         drawBackground(CREAM);
-        updateAndDrawCircle(movingCircle);
-    }
-
-    private void updateAndDrawCircle(Circle circle) {
-        updateCirclePosition(circle);
-        updateCircleColor(circle);
-        circle.draw();
-    }
-
-    private void updateCirclePosition(Circle circle) {
-        currentCirclePositionInDegree++;
-        if (currentCirclePositionInDegree > 360) {
-            currentCirclePositionInDegree = 0;
-        }
-        double x = (WINDOW_HEIGHT / 2.0) + (CIRCLE_MOVEMENT_RADIUS * Math.cos(Math.toRadians(currentCirclePositionInDegree)));
-        double y = (WINDOW_WIDTH / 2.0) + (CIRCLE_MOVEMENT_RADIUS * Math.sin(Math.toRadians(currentCirclePositionInDegree)));
-        circle.setPosition((float) x, (float) y);
-    }
-
-    private void updateCircleColor(Circle circle) {
-        if (movingCircle.getYPos() < WINDOW_HEIGHT / 2.0 && movingCircle.getXPos() > WINDOW_WIDTH / 2.0) {
-            movingCircle.setColor(RED);
-        }
-        if (movingCircle.getYPos() > WINDOW_HEIGHT / 2.0 && movingCircle.getXPos() > WINDOW_WIDTH / 2.0) {
-            movingCircle.setColor(YELLOW);
-        }
-        if (movingCircle.getYPos() > WINDOW_HEIGHT / 2.0 && movingCircle.getXPos() < WINDOW_WIDTH / 2.0) {
-            movingCircle.setColor(GREEN);
-        }
-        if (movingCircle.getYPos() < WINDOW_HEIGHT / 2.0 && movingCircle.getXPos() < WINDOW_WIDTH / 2.0) {
-            movingCircle.setColor(BLUE);
-        }
     }
 
     public static void main(String[] args) {
